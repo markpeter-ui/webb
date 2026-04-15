24cf3a15f6c7b6cf3ad7b57c4511845e80672ad0
markpeter <markpetersantossapin0810@gmail.com>
2026-04-15 16:29:38 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; ..."
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin"
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()"
+          }
+        ]
+      }
+    ];
+  }
+};
